e2ca22a3afd7c0fb1905da9d29925f866256c5e7
Braveheart ola <ola20sarumi@hotmail.com>
2020-06-13 01:31:34 +0300

    list added

--- src/components/Shop.js
+++ src/components/Shop.js
@@ -5,6 +5,11 @@ function Shop() {
   return (
     <div>
       <h1>Shop Page</h1>
+      <ul>
+        <li>Bread</li>
+        <li>Apple</li>
+        <li>Oranges</li>
+      </ul>
     </div>
   );
 }
