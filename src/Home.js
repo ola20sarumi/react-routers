869c8c66de1f1b24916ea261fc6e3d3a6bb468d8
Braveheart ola <ola20sarumi@hotmail.com>
2020-06-12 15:13:42 +0300

    First commit

---
+++ src/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function Home() {
+  return (
+    <div>
+      <h1>Home Page</h1>
+      <p>A simple daily workout journal</p>
+    </div>
+  );
+}
+
+export default Home;
---
+++ src/Nav.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import './App.css';
+import logo from "./emoj.png";
+import { Link } from "react-router-dom"
+
+function Nav() {
+    const navStyle = {
+        color: "white"
+    };
+
+  return (
+      <nav>
+    
+      <img src={logo} height="50"/>
+      <ul className="nav-links">
+          <Link style={navStyle} to="/about">
+          <li>About</li>
+          </Link>
+          <Link style={navStyle} to="/shop">
+          <li>Shop</li>
+          </Link>
+          <Link style={navStyle} to="/signup">
+          <li>Signup</li>
+          </Link>
+          <Link style={navStyle} to="/login">
+          <li>Login</li>
+          </Link>
+          
+      </ul>
+     
+        
+    
+    </nav>
+  );
+}
+
+export default Nav;
